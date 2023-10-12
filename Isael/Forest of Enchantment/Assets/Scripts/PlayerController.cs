14ea7ac348b1db3057e7192c5265203d7282d635
iisael9 <iisael090@gmail.com>
2023-10-12 10:59:22 -0700

    movement-L-R

--- Isael/Forest of Enchantment/Assets/Scripts/PlayerController.cs
+++ Isael/Forest of Enchantment/Assets/Scripts/PlayerController.cs
@@ -1,9 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
+    public float walkSpeed = 5f;
+    Vector2 moveInput;
+    public bool isMoving {  get; private set; }
+
+    Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +29,17 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(moveInput.x * walkSpeed, rb.velocity.y);
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>(); ;
+
+        isMoving = moveInput != Vector2.zero;
     }
 }
